d2ba6f46a3ef56ac57b8a307ec0eb23db8e562d7
parryying <parryying@microsoft.com>
2026-08-19 14:41:32 -0700

    Replace Instagram links with Facebook

--- data/gallery.js
+++ data/gallery.js
@@ -1,7 +1,7 @@
 /* APLS gallery data - managed in Content Studio */
 window.APLS_GALLERY = {
   instagramHeading: "Latest from APLS",
-  instagramIntro: "See more classroom moments, celebrations, and school activities from @aplsfamilies.",
-  instagramProfileUrl: "https://www.instagram.com/aplsfamilies/",
+  instagramIntro: "See more classroom moments, celebrations, and school activities on Facebook.",
+  instagramProfileUrl: "https://www.facebook.com/AsiaPacificLanguageSchool",
   instagramPosts: []
 };
